5a868be0c3d1f22ca52b72c3182259e038f0c656
Luisana Acevedo <luisanaa50@gmail.com>
2022-08-11 15:24:38 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+require('./bootstrap');
+
+import {createApp} from 'vue'
+
+import App from './layouts/App.vue'
+
+createApp(App).mount("#app")
---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Luisanasssss</h1>
+</template>
